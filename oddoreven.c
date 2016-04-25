e1c14426edace837d6af28964520f29bb119fad5
ploypailinpplm <ploy_1234ploy@hotmail.com>
2016-04-25 21:16:52 +0700

    origin

--- oddoreven.c
+++ oddoreven.c
@@ -8,13 +8,9 @@ int main()
    scanf("%d", &n);
 
    if (n%2 == 0)
-       printf("This is Even\n");
+       printf("Even\n");
    else
-       printf("This is Odd\n");
-<<<<<<< HEAD
-=======
-
->>>>>>> 5f44c5bd4a0daaee8a52b76c9012b13e9b8c29fd
+       printf("Odd\n");
 
    return 0;
 }
